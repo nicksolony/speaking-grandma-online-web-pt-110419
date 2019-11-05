ab80b5396f480648f216faa20ee94e32e42e3359
Mykola Solonyy <nicksolony@gmail.com>
2019-11-05 05:12:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 def speak_to_grandma(phrase)
   
   if phrase.upcase != phrase
-    puts "HUH?! SPEAK UP, SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
   elsif phrase = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+     "I LOVE YOU TOO PUMPKIN!"
   else
-    puts  "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   end
   
 end
